d7319f600480515f59bde609062f18ca0900febb
brandoncaulfield <19891236+brandoncaulfield@users.noreply.github.com>
2020-10-03 14:43:08 +0100

    Step 10: Descriptor for Applications

--- webapp/component.js
+++ webapp/component.js
@@ -6,12 +6,7 @@ sap.ui.define([
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata : {
-            rootView: {
-                "viewName": "sap.ui.demo.walkthrough.view.App",
-                "type": "XML",
-                "async": true,
-                "id": "app"
-            }
+            manifest: "json"
         },
         init : function () {
             // call the init function of the parent
@@ -25,13 +20,6 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
 
-            // set i18n model
-            var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-                supportedLocales: [""],
-                fallbackLocale: ""
-            });
-            this.setModel(i18nModel, "i18n")
         }
     });
 });
