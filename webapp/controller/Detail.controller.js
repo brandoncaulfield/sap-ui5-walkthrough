1f4ba8cb95fbe7583ece7d580bb761fcbf270105
brandoncaulfield <19891236+brandoncaulfield@users.noreply.github.com>
2020-10-03 15:11:42 +0100

    Step 34: Custom Controls

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/UIComponent",
-    "sap/ui/core/routing/History"
-], function (Controller, UIComponent, History) {
+    "sap/ui/core/routing/History",
+    "sap/m/MessageToast"
+], function (Controller, UIComponent, History, MessageToast) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
         onInit: function () {
@@ -10,6 +11,7 @@ sap.ui.define([
             oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
         },
         _onObjectMatched: function (oEvent) {
+            this.byId("rating").reset();
             this.getView().bindElement({
                 path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                 model: "invoice"
@@ -25,6 +27,12 @@ sap.ui.define([
                 var oRouter = UIComponent.getRouterFor(this);
                 oRouter.navTo("overview", {}, true)
             }
-        }
+        },
+        onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
+		}
     })
 })
