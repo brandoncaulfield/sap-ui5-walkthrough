30eaabd6677116e0baf0e622893ce89d6cb391bc
brandoncaulfield <19891236+brandoncaulfield@users.noreply.github.com>
2020-10-03 14:33:46 +0100

    Step 6: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App",{
         onShowHello : function () {
             // Show a native or vanailla JS alert
-            alert("Hello there!");
+            MessageToast.show("Hello there!")
         }
     });
 });
